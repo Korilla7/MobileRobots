2704b6c9514744f088305b432a1a0247014ef58a
Nikodem <medokingry@gmail.com>
2023-11-22 17:23:26 +0100

    correct visualistion of points

--- grid_map/grid_map.py
+++ grid_map/grid_map.py
@@ -33,13 +33,24 @@ for data in scan_arr:
     scan_arr[i] = data_xy 
     i=i+1
 
+ref_x = pose_arr[0][0] + 0.18
+ref_y = pose_arr[0][1]
+ref_theta = pose_arr[0][2]
+
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.axis('equal')
 
-for data_scan, data_pose in scan_arr, pose_arr:
-    plt.scatter(data_pose[0], data_pose[1], color = "red") 
+for data_scan, data_pose in zip(scan_arr, pose_arr):
+    plt.scatter(data_pose[0], data_pose[1], color = "red")
+    delta_x = data_pose[0] - ref_x
+    delta_y = data_pose[1] - ref_y
+    delta_theta = data_pose[2] - ref_theta
     for x,y in data_scan:
+        rotated_x = x * math.cos(math.radians(delta_theta)) - y * math.sin(math.radians(delta_theta))
+        rotated_y = x * math.sin(math.radians(delta_theta)) + y * math.cos(math.radians(delta_theta))
+        x = rotated_x + delta_x
+        y = rotated_y + delta_y
         plt.scatter(x,y, color="black")
 
 
